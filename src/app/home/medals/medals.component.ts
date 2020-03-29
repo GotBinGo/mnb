638e79f7395c7a9d330d576135313386dcf27c4b
Henczidai Bálint <henczidaibalint@gmail.com>
2020-03-29 18:37:37 +0200

    korok és %ok

--- src/app/home/medals/medals.component.ts
+++ src/app/home/medals/medals.component.ts
@@ -1,12 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-medals',
   templateUrl: './medals.component.html',
   styleUrls: ['./medals.component.scss']
 })
-export class MedalsComponent implements OnInit {
+export class MedalsComponent implements OnInit, AfterViewInit {
+  progresses = {
+    barter: 0,
+    shell: 0,
+    coin: 0,
+    paper: 0,
+    plastic: 0,
+    crypto: 0
+  };
+
   constructor() {}
 
   ngOnInit() {}
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.progresses = {
+        barter: 37,
+        shell: 52,
+        coin: 73,
+        paper: 100,
+        plastic: 32,
+        crypto: 12
+      };
+    }, 1000);
+  }
 }
