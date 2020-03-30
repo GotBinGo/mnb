0f74a6be44cdae25d979b315e8189f46fcfaa7aa
Bordak Tamas <tamas@webtar.hu>
2020-03-30 19:52:48 +0200

    qr open

--- src/app/shell/popup/popup.component.ts
+++ src/app/shell/popup/popup.component.ts
@@ -8,21 +8,58 @@ import { PopupService } from '@app/popup.service';
   styleUrls: ['./popup.component.scss']
 })
 export class PopupComponent implements OnInit {
+  scanned = false;
+  last = false;
+  scale = 0;
   get popup(): any {
+    // if (this.ps.popup.opened) {
+    //   setTimeout((x: any) => {
+    //     if (document.getElementById('qr')) {
+    //       document.getElementById('qr').style.transform = 'scale(1)';
+    //     }
+    //   }, 100);
+    // } else {
+    //   setTimeout((x: any) => {
+    //     this.scanned = false;
+    //   }, 100);
+    //   if (document.getElementById('qr')) {
+    //     document.getElementById('qr').style.transform = 'scale(0)';
+    //   }
+    // }
+
+    if (!this.last && this.ps.popup.opened) { // nyit
+      console.log('nyit');
+
+      setTimeout((x: any) => {
+        this.scale = 1;
+      }, 100);
+    }
+
+    if (this.last && !this.ps.popup.opened) { // csuk
+      console.log('csuk');
+      setTimeout((x: any) => {
+        this.scale = 0;
+        this.scanned = false;
+      }, 1);
+    }
+
+    this.last = this.ps.popup.opened;
     return this.ps.popup;
   }
-  normalList1 = ['p1', 'p2', 'p3'];
-
-  normalList2 = ['u1', 'u2', 'u3'];
+  normalList1 = ['p3'];
+  normalList2 = ['u2', 'p2'];
 
   normalOptions: SortablejsOptions = {
     group: 'normal-group'
   };
   constructor(private ps: PopupService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
   close() {
     this.popup.opened = false;
+    this.normalList1 = ['p3'];
+    this.normalList2 = ['u2', 'p2'];
   }
 }
